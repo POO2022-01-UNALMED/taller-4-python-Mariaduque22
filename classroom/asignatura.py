de2ced384fe771e828e0dab1de85d0a35c4ea0ba
Maria Paula Duque <92931247+Mariaduque22@users.noreply.github.com>
2022-04-05 21:11:23 -0500

    mmm

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,5 +1,9 @@
 class Asignatura:
 
+    def __str__(self):
+        completo = self._nombre+" "+self._salon
+        return completo
+        
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
